2f97dbf512db343e1ec46cfaebcdae22c0fb884c
Puro Touko O <toxpuro@cubbli20-gold.local>
2021-09-28 17:55:53 +0300

    directors for random forest and csv-files

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from pickle import TRUE
 from sqlalchemy import create_engine
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
@@ -15,14 +14,41 @@ def get_dummy_genres(genresdf):
     return genresdf.drop("genres", axis=1)
 
 def get_dummy_actors(actorsdf):
-    aggregation_functions = {"nconst": lambda x: x.tolist()}
+    aggregation_functions = {"primary_name": lambda x: x.tolist()}
     actorsdf = actorsdf.groupby(actorsdf.index).aggregate(aggregation_functions)
-    actors = actorsdf["nconst"]
+    actors = actorsdf["primary_name"]
     distinct_actors = set(list(np.concatenate(actors.values)))
 
     for actor in distinct_actors:
         actorsdf[actor] = actors.map(lambda x: int(actor in x))
-    return actorsdf.drop("nconst", axis=1)
+    return actorsdf.drop("primary_name", axis=1)
+
+def get_actors():
+    load_dotenv()
+    DB_URI = getenv("DATABASE_URI")
+    connection = create_engine(DB_URI)
+    sql = "SELECT DISTINCT primary_name FROM title_principals INNER JOIN name_basics ON name_basics.nconst = title_principals.nconst WHERE category='actor'"
+    df = pd.read_sql_query(sql, con=connection)
+    df.to_csv("actors.csv")
+
+def get_genres():
+    load_dotenv()
+    DB_URI = getenv("DATABASE_URI")
+    connection = create_engine(DB_URI)
+    sql = "SELECT DISTINCT genres FROM title_basics"
+    df = pd.read_sql_query(sql, con=connection)
+    df["genres"] = df["genres"].str.split(",")
+    df = df.explode("genres")
+    genres = df["genres"].drop_duplicates()
+    genres.to_csv("genres.csv")
+
+def get_directors():
+    load_dotenv()
+    DB_URI = getenv("DATABASE_URI")
+    connection = create_engine(DB_URI)
+    sql = "SELECT DISTINCT primary_name FROM title_principals INNER JOIN name_basics ON name_basics.nconst = title_principals.nconst WHERE category='director'"
+    df = pd.read_sql_query(sql, con=connection)
+    df.to_csv("directors.csv")
 
 def main():
 
@@ -31,13 +57,15 @@ def main():
     load_dotenv()
     DB_URI = getenv("DATABASE_URI")
     connection = create_engine(DB_URI)
-    sql = 'SELECT genres, title_principals.nconst, title_basics.tconst, average_rating FROM title_basics INNER JOIN title_ratings ON title_ratings.tconst = title_basics.tconst LEFT JOIN title_principals ON title_principals.tconst = title_basics.tconst LIMIT 20'
+    sql = "SELECT genres, name_basics.primary_name, title_basics.tconst, average_rating FROM title_basics INNER JOIN title_ratings ON title_ratings.tconst = title_basics.tconst LEFT JOIN title_principals ON title_principals.tconst = title_basics.tconst INNER JOIN name_basics ON name_basics.nconst = title_principals.nconst WHERE title_principals.category='actor' OR title_principals.category='director' LIMIT 20"
     df = pd.read_sql_query(sql, con=connection)
     df.set_index("tconst", inplace=True)
     genres = df[["genres"]]
     dummy_genres = get_dummy_genres(genres[~genres.index.duplicated(keep='first')])
-    dummy_actors = get_dummy_actors(df[["nconst"]])
-    X = pd.concat([dummy_genres, dummy_actors], axis=1)
+    ## dummy_people includes actors and directors
+    dummy_people = get_dummy_actors(df[["primary_name"]])
+    
+    X = pd.concat([dummy_genres, dummy_people], axis=1)
     avg_ratings = df[["average_rating"]]
     Y = avg_ratings[~avg_ratings.index.duplicated(keep='first')]
     regr = RandomForestRegressor(max_depth=2, random_state=0)
@@ -45,6 +73,8 @@ def main():
     joblib.dump(regr, "./random_forest.joblib")
     loaded_rf = joblib.load("./random_forest.joblib")
     print(loaded_rf.predict([X.iloc[0,:]]))
+
+    get_directors()
 
 if __name__=="__main__":
     main()
